3c76049323a15b2e9707426d569904904e6a313b
Aqdas-7 <aq10ali8@gmail.com>
2024-02-14 21:10:11 +0530

    New Commit

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import { MultiChatSocket, MultiChatWindow, useMultiChatLogic} from "react-chat-engine-advanced";
+import { MultiChatScoket, MultiChatWindow, useMultiChatLogic} from "react-chat-engine-advanced";
 
 const ChatsPage = (props) => {
     const chatProps = useMultiChatLogic('81535517-3a7f-4e19-bc24-1bca227b444a', props.user.username, props.user.secret);
